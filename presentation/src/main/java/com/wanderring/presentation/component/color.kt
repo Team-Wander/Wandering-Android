7a13fffc1647b087438d5eb9d9d4c31701384ffc
혜성 <118062596+answad@users.noreply.github.com>
2024-05-27 21:10:43 +0900

    :lipstick: :: color 추가

---
+++ presentation/src/main/java/com/wanderring/presentation/component/color.kt
@@ -0,0 +1,23 @@
+package com.wanderring.presentation.component
+
+import androidx.compose.ui.graphics.Color
+
+object DoColor {
+    val MAIN = Color(0xFF36C05C)
+    val Black = Color(0xFF333333)
+    val WHITE = Color(0xFFFFFFFF)
+    val RED = Color(0xFFE53A3A)
+
+    val GRAY900 = Color(0xFF4F4F51)
+    val GRAY700 = Color(0xFF828387)
+    val GRAY500 = Color(0xFFA5A6A9)
+    val GRAY400 = Color(0xFFB4B5B7)
+    val GRAY200 = Color(0xFFEFF0F2)
+
+
+    val CAT_BLUE = Color(0xFF7CAAEF)
+    val CAT_PURPLE = Color(0xFF9A7CEF)
+    val CAT_YELLOW = Color(0xFFF5C185)
+    val CAT_RED = Color(0xFFFF6868)
+    val CAT_GRAY = Color(0xFF868686)
+}
