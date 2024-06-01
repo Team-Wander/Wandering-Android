8e9af38f1cae6b70bbeef0d66b7d9a982693cde3
혜성 <118062596+answad@users.noreply.github.com>
2024-06-01 16:29:25 +0900

    :lipstick: :: color과 text를 입력받지 않도록 변경,isSelected로 버튼이 눌렸는지 안눌렸는지 확인

--- presentation/src/main/java/com/wanderring/presentation/component/DoCategoryButton.kt
+++ presentation/src/main/java/com/wanderring/presentation/component/DoCategoryButton.kt
@@ -1,5 +1,6 @@
 package com.wanderring.presentation.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -14,39 +15,58 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.wanderring.domain.model.enum.Tag
 import com.wanderring.presentation.component.theme.DoColor
 import com.wanderring.presentation.component.theme.DoTypography
 
 @Composable
 fun DoCategoryButton(
     modifier: Modifier = Modifier,
-    color: Color,
-    text: String,
+    tag: Tag,
+    isSelected: Boolean = false,
 ) {
     Row(
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier.border(
-            width = 1.dp,
-            color = color,
-            shape = RoundedCornerShape(size = 8.dp)
-        )
+        modifier = modifier
+            .then(
+                if (isSelected) Modifier.background(
+                    color = Color(tag.colorCode), shape = RoundedCornerShape(size = 8.dp)
+                )
+                else Modifier
+            )
+            .border(
+                width = 1.dp,
+                color = Color(tag.colorCode),
+                shape = RoundedCornerShape(size = 8.dp)
+            )
     ) {
         Text(
-            text = text,
+            text = tag.description,
             style = DoTypography.labelLarge,
             fontWeight = FontWeight(600),
-            color = color
+            color = if (isSelected) DoColor.WHITE else Color(tag.colorCode)
         )
     }
 }
 
 @Preview
 @Composable
-private fun Preview() {
+private fun PreviewNotSelected() {
     DoCategoryButton(
-        color = DoColor.CAT_RED,
-        text = "텍스트입니다",
+        tag = Tag.WORRY,
+        isSelected = false,
+        modifier = Modifier
+            .height(70.dp)
+            .width(400.dp)
+    )
+}
+@Preview
+@Composable
+private fun PreviewSelected() {
+    DoCategoryButton(
+        tag = Tag.WORRY,
+        isSelected = true,
         modifier = Modifier
             .height(70.dp)
             .width(400.dp)
