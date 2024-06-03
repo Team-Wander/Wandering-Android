a3e13ce5f01cf7f9ae4d3d34319111f73f387778
혜성 <118062596+answad@users.noreply.github.com>
2024-06-04 08:13:45 +0900

    EnterSchoolRoute의 navigation 추가

--- presentation/src/main/java/com/wanderring/presentation/view/enterInfo/EnterInfoNavi.kt
+++ presentation/src/main/java/com/wanderring/presentation/view/enterInfo/EnterInfoNavi.kt
@@ -1,2 +1,24 @@
 package com.wanderring.presentation.view.enterInfo
 
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.wanderring.presentation.view.enterInfo.page.EnterSchoolRoute
+
+const val EnterInfo = "Enter_Info"
+
+fun NavController.navigationToEnterInfo() {
+    this.navigate(EnterInfo)
+}
+
+fun NavGraphBuilder.enterInfo(
+    navigateToHome: () -> Unit,
+    navigateToBack: () -> Unit,
+) {
+    composable(EnterInfo) {
+        EnterSchoolRoute(
+            navigateToHome = navigateToHome,
+            navigateToBack = navigateToBack
+        )
+    }
+}
