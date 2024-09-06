b8e1108a5cc794560539993ecd8c1663726b9f35
youloveli <mymhj122@gmail.com>
2024-09-06 15:42:49 +0900

    NiaAppState 생성

---
+++ app/src/main/java/com/wanderring/Do/ui/NiaAppState.kt
@@ -0,0 +1,48 @@
+package com.wanderring.Do.ui
+
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.navigation.NavDestination
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.navOptions
+import com.wanderring.Do.navigation.TopLevelDestination
+
+@Stable
+class NiaAppState(val navController: NavHostController) {
+    val currentDestination: NavDestination?
+        @Composable get() = navController
+            .currentBackStackEntryAsState().value?.destination
+
+    val currentTopLevelDestination: TopLevelDestination?
+        @Composable get() = when (currentDestination?.route) {
+            // todo
+            else -> null
+        }
+
+
+    val topLevelDestinations: List<TopLevelDestination> = TopLevelDestination.values().toList()
+
+    fun navigateToTopLevelDestination(topLevelDestination: TopLevelDestination) {
+        Log.d("Navigation", "Navigation: ${topLevelDestination.name}")
+
+        val topLevelNavOptions = navOptions {
+            popUpTo(navController.graph.findStartDestination().id) {
+                saveState = true
+            }
+
+            launchSingleTop = true
+
+            restoreState = true
+        }
+
+        when (topLevelDestination) {
+            TopLevelDestination.Home -> TODO()
+            TopLevelDestination.Search -> TODO()
+            TopLevelDestination.Schedule -> TODO()
+            TopLevelDestination.MY -> TODO()
+        }
+    }
+}
