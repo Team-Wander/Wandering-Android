85448bc90354fc9325214617a859ec4865a2a001
youloveli <mymhj122@gmail.com>
2024-09-06 15:43:03 +0900

    TopLevelDestination 생성

---
+++ app/src/main/java/com/wanderring/Do/navigation/TopLevelDestination.kt
@@ -0,0 +1,25 @@
+package com.wanderring.Do.navigation
+
+import androidx.compose.runtime.Composable
+
+enum class TopLevelDestination(
+    val icon: @Composable () -> Unit,
+    val destinationName: String,
+) {
+    Home(
+        icon = { com.wanderring.presentation.component.HomeIcon() },
+        destinationName = "홈"
+    ),
+    Search(
+        icon = { com.wanderring.presentation.component.SearchIcon() },
+        destinationName = "검색"
+    ),
+    Schedule(
+        icon = { com.wanderring.presentation.component.TimeScheduleIcon() },
+        destinationName = "시간표"
+    ),
+    MY(
+        icon = { com.wanderring.presentation.component.MyIcon() },
+        destinationName = "마이"
+    ),
+}
