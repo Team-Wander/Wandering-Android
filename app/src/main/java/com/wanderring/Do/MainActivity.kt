7f74fd024699b716d946e212c95ec1c53a6a4f8e
youloveli <mymhj122@gmail.com>
2024-09-07 18:52:15 +0900

    MainActivity에  App 적용

--- app/src/main/java/com/wanderring/Do/MainActivity.kt
+++ app/src/main/java/com/wanderring/Do/MainActivity.kt
@@ -3,12 +3,18 @@ package com.wanderring.Do
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.wanderring.Do.ui.App
+import com.wanderring.Do.ui.rememberAppState
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            DoNavHost(startDestination = "")
+            val appState = rememberAppState()
+
+            App(appState = appState)
         }
     }
 }
