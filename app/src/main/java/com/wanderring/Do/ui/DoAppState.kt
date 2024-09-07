f71de03107f50eab60a3ce69a189e568f304c60c
youloveli <mymhj122@gmail.com>
2024-09-07 18:15:51 +0900

    rememberDoAppState 구현

--- app/src/main/java/com/wanderring/Do/ui/DoAppState.kt
+++ app/src/main/java/com/wanderring/Do/ui/DoAppState.kt
@@ -3,15 +3,26 @@ package com.wanderring.Do.ui
 import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
 import androidx.navigation.NavDestination
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navOptions
 import com.wanderring.Do.navigation.TopLevelDestination
 
+@Composable
+fun rememberDoAppState(
+    navController: NavHostController = rememberNavController(),
+): DoAppState {
+    return remember(navController) {
+        DoAppState(navController = navController)
+    }
+}
+
 @Stable
-class NiaAppState(val navController: NavHostController) {
+class DoAppState(val navController: NavHostController) {
     val currentDestination: NavDestination?
         @Composable get() = navController
             .currentBackStackEntryAsState().value?.destination
