047b97cad5dd5ee2266351dc0f804e50204f7c66
youloveli <mymhj122@gmail.com>
2024-09-11 00:50:54 +0900

    주석 추가

--- app/src/main/java/com/wanderring/Do/app/AppState.kt
+++ app/src/main/java/com/wanderring/Do/app/AppState.kt
@@ -20,6 +20,7 @@ import com.wanderring.presentation.view.my.navigateToMyRoute
 import com.wanderring.presentation.view.schedule.navigateToScheduleRoute
 import com.wanderring.presentation.view.search.navigateToSearchRoute
 
+// Compose에서 상태를 유지하기 위한 AppState 객체를 생성하는 함수
 @Composable
 fun rememberAppState(
     navController: NavHostController = rememberNavController(),
@@ -29,28 +30,32 @@ fun rememberAppState(
     }
 }
 
+// 앱의 네비게이션 상태를 관리하고, 최상위 목적지로의 이동을 구현한 클래스
 @Stable
 class AppState(val navController: NavHostController) {
+
+    // 현재 네비게이션 백스택의 최상위 항목의 목적지
     val currentDestination: NavDestination?
         @Composable get() = navController
             .currentBackStackEntryAsState().value?.destination
 
+    // 현재 목적지가 최상위 목적지 중 하나인지 여부
     val isTopLevelDestination: Boolean
         @Composable get() = TopLevelDestination.values()
             .any { currentDestination?.route == it.destinationName }
 
+    // 모든 최상위 목적지의 리스트
     val topLevelDestinations: List<TopLevelDestination> = TopLevelDestination.entries
 
+    // 최상위 목적지로 네비게이션
     fun navigateToTopLevelDestination(topLevelDestination: TopLevelDestination) {
-        Log.d("Navigation", "Navigation: ${topLevelDestination.name}")
+        Log.d("Navigation", "Navigating to: ${topLevelDestination.name}")
 
         val topLevelNavOptions = navOptions {
             popUpTo(navController.graph.findStartDestination().id) {
                 saveState = true
             }
-
             launchSingleTop = true
-
             restoreState = true
         }
 
