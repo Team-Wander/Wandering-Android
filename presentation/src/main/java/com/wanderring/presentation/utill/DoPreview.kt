e63071aae59df934de484dec6bcf4d58ab8008fd
youloveli <mymhj122@gmail.com>
2024-09-10 09:08:18 +0900

    annotation DoPreview 추가

---
+++ presentation/src/main/java/com/wanderring/presentation/utill/DoPreview.kt
@@ -0,0 +1,7 @@
+package com.wanderring.presentation.utill
+
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+annotation class DoPreview()
