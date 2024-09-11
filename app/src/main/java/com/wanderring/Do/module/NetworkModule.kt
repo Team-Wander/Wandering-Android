c007881ece5886dbcfa0f6907200eae986a50ed5
youloveli <mymhj122@gmail.com>
2024-09-11 23:55:07 +0900

    NetworkModule 추가

---
+++ app/src/main/java/com/wanderring/Do/module/NetworkModule.kt
@@ -0,0 +1,68 @@
+package com.wanderring.Do.module
+
+import android.util.Log
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+private object NetworkModule {
+
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor { message -> Log.v("HTTP", message) }
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    @Provides
+    @Singleton
+    fun provideOkhttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+    ) = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .addInterceptor(httpLoggingInterceptor)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideMoshiInstance(): Moshi =
+        Moshi
+            .Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideMoshiJsonAdapterInstance(moshi: Moshi): JsonAdapter<String> =
+        moshi.adapter(String::class.java)
+
+
+    @Provides
+    @Singleton
+    fun provideConverterFactory(moshi: Moshi): MoshiConverterFactory =
+        MoshiConverterFactory.create(moshi)
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        moshiConverterFactory: MoshiConverterFactory,
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl("BuildConfig.BASE_URL")
+        .client(okHttpClient)
+        .addConverterFactory(moshiConverterFactory)
+        .build()
+}
