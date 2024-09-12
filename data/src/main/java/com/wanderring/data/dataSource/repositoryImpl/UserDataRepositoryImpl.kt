c8f6c1ab57ee5d57e1fdd73d4c322bd68b9f6122
youloveli <mymhj122@gmail.com>
2024-09-12 20:43:05 +0900

    UserDataRepository 추가

---
+++ data/src/main/java/com/wanderring/data/dataSource/repositoryImpl/UserDataRepositoryImpl.kt
@@ -0,0 +1,62 @@
+package com.wanderring.data.dataSource.repositoryImpl
+
+import com.wanderring.data.dataSource.EncryptedSharedPreferencesDataSource
+import com.wanderring.domain.model.repository.UserDataRepository
+import javax.inject.Inject
+
+class UserDataRepositoryImpl @Inject constructor(
+    private val encryptedSharedPreferencesDataSource: EncryptedSharedPreferencesDataSource
+) : UserDataRepository {
+    override fun getAccessToken(): String? = encryptedSharedPreferencesDataSource.getAccessToken()
+
+    override fun getAccessTime(): String? = encryptedSharedPreferencesDataSource.getAccessTime()
+
+    override fun getRefreshToken(): String? = encryptedSharedPreferencesDataSource.getRefreshToken()
+
+    override fun getRefreshTime(): String? = encryptedSharedPreferencesDataSource.getRefreshTime()
+
+    override fun setAccess(accessToken: String, accessTime: String) {
+        with(encryptedSharedPreferencesDataSource) {
+            setAccessToken(accessToken)
+            setAccessTime(accessTime)
+        }
+    }
+
+    override fun setRefresh(refreshToken: String, refreshTime: String) {
+        with(encryptedSharedPreferencesDataSource) {
+            setRefreshTime(refreshTime)
+            setRefreshToken(refreshToken)
+        }
+    }
+
+    override fun deleteAccess() {
+        with(encryptedSharedPreferencesDataSource) {
+            deleteAccessToken()
+            deleteAccessTime()
+        }
+    }
+
+    override fun deleteRefresh() {
+        with(encryptedSharedPreferencesDataSource) {
+            deleteRefreshToken()
+            deleteRefreshTime()
+        }
+    }
+
+    override fun getIsOnBoardingFinished(): Boolean =
+        encryptedSharedPreferencesDataSource.getIsOnBoardingFinished() ?: true
+
+    override fun setIsOnBoardingFinished() =
+        encryptedSharedPreferencesDataSource.setIsOnBoardingFinished()
+
+    override fun logOut() {
+        with(encryptedSharedPreferencesDataSource) {
+            deleteAccessToken()
+            deleteAccessTime()
+            deleteRefreshToken()
+            deleteRefreshTime()
+            deleteIsOnBoardingFinished()
+        }
+    }
+
+}
---
+++ domain/src/main/java/com/wanderring/domain/model/repository/UserDataRepository.kt
@@ -0,0 +1,21 @@
+package com.wanderring.domain.model.repository
+
+interface UserDataRepository {
+    fun getAccessToken(): String?
+    fun getAccessTime(): String?
+
+    fun getRefreshToken(): String?
+    fun getRefreshTime(): String?
+
+    fun setAccess(accessToken: String, accessTime: String)
+    fun setRefresh(refreshToken: String, refreshTime: String)
+
+    fun deleteAccess()
+    fun deleteRefresh()
+
+    fun getIsOnBoardingFinished(): Boolean
+
+    fun setIsOnBoardingFinished()
+
+    fun logOut()
+}
