2c700b9cf7434d87f993e5d2c91a9e3f66339cd5
youloveli <mymhj122@gmail.com>
2024-09-14 20:50:54 +0900

    DoNavHost 에서 appState 파라미터 삭제

--- app/src/main/java/com/wanderring/Do/navigation/DoNavHost.kt
+++ app/src/main/java/com/wanderring/Do/navigation/DoNavHost.kt
@@ -1,17 +1,15 @@
 package com.wanderring.Do.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import com.wanderring.Do.app.AppState
 import com.wanderring.presentation.view.onboarding.onBoarding
 
 @Composable
 fun DoNavHost(
-    appState: AppState,
     startDestination: String,
+    navController: NavHostController,
 ) {
-    val navController = appState.navController
-
     NavHost(
         navController = navController,
         startDestination = startDestination
