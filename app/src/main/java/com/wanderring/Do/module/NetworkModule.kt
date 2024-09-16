404f91bde4d7ba43f20afa3aa6b11f6c8cf220b8
youloveli <mymhj122@gmail.com>
2024-09-16 11:56:55 +0900

    BuildConfig 추가

--- app/src/main/java/com/wanderring/Do/module/NetworkModule.kt
+++ app/src/main/java/com/wanderring/Do/module/NetworkModule.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.wanderring.Do.BuildConfig
 import com.wanderring.data.utill.TokenInterceptor
 import dagger.Module
 import dagger.Provides
@@ -64,7 +65,7 @@ private object NetworkModule {
         okHttpClient: OkHttpClient,
         moshiConverterFactory: MoshiConverterFactory,
     ): Retrofit = Retrofit.Builder()
-        .baseUrl("BuildConfig.BASE_URL")
+        .baseUrl(BuildConfig.BASE_URL)
         .client(okHttpClient)
         .addConverterFactory(moshiConverterFactory)
         .build()
