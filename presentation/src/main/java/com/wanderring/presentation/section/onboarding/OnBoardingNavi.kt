f947bb753b845e256be8c4196859d1f0297f433c
youloveli <mymhj122@gmail.com>
2024-09-17 22:43:31 +0900

    page -> screen 이동

--- presentation/src/main/java/com/wanderring/presentation/section/onboarding/OnBoardingNavi.kt
+++ presentation/src/main/java/com/wanderring/presentation/section/onboarding/OnBoardingNavi.kt
@@ -4,7 +4,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import com.wanderring.presentation.section.onboarding.page.OnBoardingRoute
+import com.wanderring.presentation.section.onboarding.screen.OnBoardingRoute
 
 const val OnBoardingRoute = "OnBoardingRoute"
 const val LoginRoute = "LoginRoute"
