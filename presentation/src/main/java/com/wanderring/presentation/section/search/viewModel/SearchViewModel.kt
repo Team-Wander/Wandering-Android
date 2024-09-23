29fb9f072217f740d7ed676cd5ccd56cc9401ddd
youloveli <mymhj122@gmail.com>
2024-09-23 20:19:10 +0900

    SearchViewModel 구현

---
+++ presentation/src/main/java/com/wanderring/presentation/section/search/viewModel/SearchViewModel.kt
@@ -0,0 +1,56 @@
+package com.wanderring.presentation.section.search.viewModel
+
+import DoViewModel
+import com.wanderring.presentation.section.home.component.DoWalkListItemState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.immutableListOf
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+
+) : DoViewModel<SearchScreenIntent, SearchScreenState, SearchSideEffect>(SearchScreenState.getInitialState()) {
+
+    override fun handleIntent(intent: SearchScreenIntent) {
+        when (intent) {
+            SearchScreenIntent.LoadSearchResult -> loadSearchResult()
+            SearchScreenIntent.SetSearchTextState -> setSearchTextState(state.value.searchTextState)
+            SearchScreenIntent.NavigateToBackStack -> SearchSideEffect.NavigateToBackStack
+            SearchScreenIntent.NavigateToSeekWritingDetail -> SearchSideEffect.NavigateToSeekWritingDetail
+        }
+    }
+
+    private fun loadSearchResult() {
+        // TODO:
+    }
+
+    private fun setSearchTextState(textState: String) {
+        // TODO:
+    }
+}
+
+sealed class SearchScreenIntent {
+    data object NavigateToSeekWritingDetail : SearchScreenIntent()
+    data object LoadSearchResult : SearchScreenIntent()
+    data object SetSearchTextState : SearchScreenIntent()
+    data object NavigateToBackStack : SearchScreenIntent()
+}
+
+data class SearchScreenState(
+    val searchTextState: String,
+    val seekList: ImmutableList<DoWalkListItemState>,
+) {
+    companion object {
+        // State의 초기값을 넣어주기위해 필수로 구현해야하는 함수
+        fun getInitialState() = SearchScreenState(
+            searchTextState = "",
+            seekList = immutableListOf(),
+        )
+    }
+}
+
+sealed class SearchSideEffect {
+    data object NavigateToSeekWritingDetail : SearchSideEffect()
+    data object NavigateToBackStack : SearchSideEffect()
+}
