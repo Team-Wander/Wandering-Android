d3e34cb9815e2a925c7661b83af3a44002fee068
youloveli <mymhj122@gmail.com>
2024-09-26 00:09:56 +0900

    Binds 추가

--- app/src/main/java/com/wanderring/Do/module/RepositoryModule.kt
+++ app/src/main/java/com/wanderring/Do/module/RepositoryModule.kt
@@ -2,6 +2,7 @@ package com.wanderring.Do.module
 
 import com.wanderring.data.dataSource.repositoryImpl.UserDataRepositoryImpl
 import com.wanderring.domain.model.repository.UserDataRepository
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -9,6 +10,8 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
+
+    @Binds
     abstract fun provideUserDataRepository(
         userDataRepositoryImpl: UserDataRepositoryImpl
     ): UserDataRepository
