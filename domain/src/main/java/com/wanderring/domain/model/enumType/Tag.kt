2002911bb7825098cfcf7a03ca9832fa7d4f8442
youloveli <mymhj122@gmail.com>
2024-09-25 14:12:06 +0900

    descriptionWithHash 추가

--- domain/src/main/java/com/wanderring/domain/model/enumType/Tag.kt
+++ domain/src/main/java/com/wanderring/domain/model/enumType/Tag.kt
@@ -1,10 +1,13 @@
 package com.wanderring.domain.model.enumType
 
-enum class Tag(val description: String) {
-    WALK(description = "#산책"),
-    WORRY(description = "#고민"),
-    CHAT(description = "#잡담"),
-    EXERCISE(description = "#외출"),
-    STUDY(description = "#운동"),
-    GO_OUT(description = "#공부"),
+enum class Tag(
+    val description: String,
+    val descriptionWithHash: String,
+) {
+    WALK(description = "산책", descriptionWithHash = "#산책"),
+    WORRY(description = "고민", descriptionWithHash = "#고민"),
+    CHAT(description = "잡담", descriptionWithHash = "#잡담"),
+    EXERCISE(description = "외출", descriptionWithHash = "#외출"),
+    STUDY(description = "운동", descriptionWithHash = "#운동"),
+    GO_OUT(description = "공부", descriptionWithHash = "#공부"),
 }
