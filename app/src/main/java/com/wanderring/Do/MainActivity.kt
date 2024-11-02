02641e6810856b304323065173121cba965b7dc2
youloveli <mymhj122@gmail.com>
2024-11-02 20:36:08 +0900

    AndroidEntryPoint 어노테이션 추가

--- app/src/main/java/com/wanderring/Do/MainActivity.kt
+++ app/src/main/java/com/wanderring/Do/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.compose.setContent
 import com.wanderring.Do.app.App
 import com.wanderring.Do.app.rememberAppState
 import com.wanderring.domain.model.repository.UserDataRepository
+import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @Inject
     lateinit var userDataRepository: UserDataRepository
