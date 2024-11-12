5effcb4e476766aa32aab281205bd973d8a75b3b
youloveli <mymhj122@gmail.com>
2024-11-12 20:33:52 +0900

    잘못된 description 추가

--- domain/src/main/java/com/wanderring/domain/model/enumType/Tag.kt
+++ domain/src/main/java/com/wanderring/domain/model/enumType/Tag.kt
@@ -7,7 +7,7 @@ enum class Tag(
     WALK(description = "산책", descriptionWithHash = "#산책"),
     WORRY(description = "고민", descriptionWithHash = "#고민"),
     CHAT(description = "잡담", descriptionWithHash = "#잡담"),
-    STUDY(description = "운동", descriptionWithHash = "#운동"),
-    GO_OUT(description = "공부", descriptionWithHash = "#공부"),
-    EXERCISE(description = "외출", descriptionWithHash = "#외출"),
+    STUDY(description = "공부", descriptionWithHash = "#공부"),
+    GO_OUT(description = "외출", descriptionWithHash = "#외출"),
+    EXERCISE(description = "운동", descriptionWithHash = "#운동"),
 }
