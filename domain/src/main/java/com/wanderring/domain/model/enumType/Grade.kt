094a577269759472422e76cfebd9bd73d918cbc8
youloveli <mymhj122@gmail.com>
2024-11-12 20:39:42 +0900

    Grade descriptionWithHash 추가

--- domain/src/main/java/com/wanderring/domain/model/enumType/Grade.kt
+++ domain/src/main/java/com/wanderring/domain/model/enumType/Grade.kt
@@ -1,12 +1,15 @@
 package com.wanderring.domain.model.enumType
 
-enum class Grade(val description: String) {
-    ONE(description = "1학년"),
-    TWO(description = "2학년"),
-    THREE(description = "3학년"),
-    FOUR(description = "4학년"),
-    FIVE(description = "5학년"),
-    SIX(description = "6학년"),
-    ALL(description = "전학년"),
-    NONE(description = "")
+enum class Grade(
+    val description: String,
+    val descriptionWithHash: String,
+) {
+    ONE(description = "#1학년", descriptionWithHash = "#1학년"),
+    TWO(description = "2학년", descriptionWithHash = "#2학년"),
+    THREE(description = "3학년", descriptionWithHash = "#3학년"),
+    FOUR(description = "4학년", descriptionWithHash = "#4학년"),
+    FIVE(description = "5학년", descriptionWithHash = "#5학년"),
+    SIX(description = "6학년", descriptionWithHash = "#6학년"),
+    ALL(description = "전학년", descriptionWithHash = "#전학년"),
+    NONE(description = "", descriptionWithHash = ""),
 }
