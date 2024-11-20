0459399aeaa89249e1148bd5aa034a26f1a0ebca
youloveli <mymhj122@gmail.com>
2024-11-20 20:58:26 +0900

    viewModel 수정

--- presentation/src/main/java/com/wanderring/presentation/section/onboarding/viewModel/OnBoardingViewModel.kt
+++ presentation/src/main/java/com/wanderring/presentation/section/onboarding/viewModel/OnBoardingViewModel.kt
@@ -1,13 +1,17 @@
 package com.wanderring.presentation.section.onboarding.viewModel
 
+import androidx.lifecycle.viewModelScope
 import com.wanderring.domain.model.enumType.Grade
+import com.wanderring.domain.model.model.address.AddressModel
+import com.wanderring.domain.model.repository.address.AddressRepository
 import com.wanderring.presentation.utill.DoViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class OnBoardingViewModel @Inject constructor(
-
+    private val addressRepository: AddressRepository,
 ) : DoViewModel<OnBoardingScreenIntent, OnBoardingScreenState, OnBoardingSideEffect>(
     OnBoardingScreenState.getInitialState()
 ) {
@@ -17,12 +21,31 @@ class OnBoardingViewModel @Inject constructor(
             is OnBoardingScreenIntent.UpdateSchool -> updateSchool(intent.school)
             is OnBoardingScreenIntent.UpdateGrade -> updateGrade(intent.grade)
             is OnBoardingScreenIntent.UpdateSpot -> updateSpot(intent.spot)
+            is OnBoardingScreenIntent.UpdateSearchText -> updateSearchTextState(intent.searchText)
+            is OnBoardingScreenIntent.SearchLocation -> updateSpot(intent.searchText)
         }
     }
 
     private fun updateSchool(school: String) = setState { copy(school = school) }
     private fun updateGrade(grade: Grade) = setState { copy(grade = grade) }
     private fun updateSpot(spot: String) = setState { copy(spot = spot) }
+    private fun updateSearchTextState(searchTextState: String) =
+        setState { copy(searchTextState = searchTextState) }
+
+    private fun searchLocation(searchTextState: String) = viewModelScope.launch {
+        addressRepository.getAddress(
+            currentPage = 1,
+            countPerPage = 5,
+            keyword = searchTextState
+        ).collect {
+            setState {
+                copy(
+                    searchList = it
+                )
+            }
+        }
+
+    }
 
     private fun postInfo() {
         // TODO: 통신 구현
@@ -34,6 +57,8 @@ sealed class OnBoardingScreenIntent {
     data class UpdateSchool(val school: String) : OnBoardingScreenIntent()
     data class UpdateGrade(val grade: Grade) : OnBoardingScreenIntent()
     data class UpdateSpot(val spot: String) : OnBoardingScreenIntent()
+    data class UpdateSearchText(val searchText: String) : OnBoardingScreenIntent()
+    data class SearchLocation(val searchText: String) : OnBoardingScreenIntent()
     data object PostInfo : OnBoardingScreenIntent()
 }
 
@@ -41,6 +66,8 @@ data class OnBoardingScreenState(
     val school: String,
     val grade: Grade,
     val spot: String,
+    val searchTextState: String,
+    val searchList: AddressModel,
 ) {
     companion object {
         // State의 초기값을 넣어주기위해 필수로 구현해야하는 함수
@@ -48,6 +75,8 @@ data class OnBoardingScreenState(
             school = "",
             grade = Grade.NONE,
             spot = "",
+            searchTextState = "",
+            searchList = AddressModel(juso = emptyList()),
         )
     }
 }
