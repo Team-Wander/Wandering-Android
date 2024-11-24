e503752f6afa2b2908a7b3be51860626f0435a38
youloveli <mymhj122@gmail.com>
2024-11-24 22:28:52 +0900

    주소api baseUrl을 사용하는 retrofit과 기본 baseUrl을 사용하는 retrofit을 hilt가 구분할수있도록 qualifier 추가

---
+++ app/src/main/java/com/wanderring/Do/module/util/RetrofitQualifier.kt
@@ -0,0 +1,11 @@
+package com.wanderring.Do.module.util
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DefaultRetrofit
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class AddressRetrofit
