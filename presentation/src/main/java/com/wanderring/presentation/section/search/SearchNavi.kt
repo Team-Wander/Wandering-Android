f461260a4ab8510ae3375b7192fad19471bc0075
youloveli <mymhj122@gmail.com>
2024-11-25 10:16:46 +0900

    writeWatchRoute 추가

--- presentation/src/main/java/com/wanderring/presentation/section/search/SearchNavi.kt
+++ presentation/src/main/java/com/wanderring/presentation/section/search/SearchNavi.kt
@@ -4,15 +4,28 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.wanderring.presentation.section.home.screen.WriteRoute
+import com.wanderring.presentation.section.search.screen.SearchRoute
 
 const val SearchRoute = "SearchRoute"
+const val WriteWatchRoute = "WriteWatchRoute"
 
 fun NavController.navigateToSearchRoute(navOptions: NavOptions? = null) {
     this.navigate(SearchRoute, navOptions)
 }
 
+fun NavController.navigateToWriteRoute(navOptions: NavOptions? = null) {
+    this.navigate(WriteWatchRoute, navOptions)
+}
+
 fun NavGraphBuilder.searchRoute() {
     composable(SearchRoute) {
+        SearchRoute()
+    }
+}
 
+fun NavGraphBuilder.writeWatchRoute() {
+    composable(WriteWatchRoute) {
+        WriteRoute()
     }
 }
