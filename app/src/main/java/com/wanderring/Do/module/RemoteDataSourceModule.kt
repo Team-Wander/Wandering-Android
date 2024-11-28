69a750c1c88f467e83b7d2019cb75f720e5bd4bc
youloveli <mymhj122@gmail.com>
2024-11-28 20:14:25 +0900

    바인딩함수명 수정,

--- app/src/main/java/com/wanderring/Do/module/RemoteDataSourceModule.kt
+++ app/src/main/java/com/wanderring/Do/module/RemoteDataSourceModule.kt
@@ -2,6 +2,8 @@ package com.wanderring.Do.module
 
 import com.wanderring.data.common.address.AddressDataSource
 import com.wanderring.data.common.address.AddressDataSourceImpl
+import com.wanderring.data.common.auth.AuthDataSource
+import com.wanderring.data.common.auth.AuthDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -11,7 +13,7 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 abstract class RemoteDataSourceModule {
     @Binds
-    abstract fun provideRemoteAddressDataSource(
-        remoteAuthDataSourceImpl: AddressDataSourceImpl
+    abstract fun provideAddressDataSource(
+        addressDataSourceImpl: AddressDataSourceImpl
     ): AddressDataSource
 }
