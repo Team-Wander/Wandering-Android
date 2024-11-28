ab954e1d5dccc0a8d437db92541f4012ace49c65
youloveli <mymhj122@gmail.com>
2024-11-28 14:19:06 +0900

    AuthApi dto 추가

--- data/src/main/java/com/wanderring/data/common/auth/AuthApi.kt
+++ data/src/main/java/com/wanderring/data/common/auth/AuthApi.kt
@@ -19,3 +19,22 @@ interface AuthApi {
     @POST("/auth/info")
     suspend fun submitExtraData(@Body extraDataBody: ExtraDataBody): TokenResponse
 }
+
+data class LoginBody(
+    val authorizationCode: String,
+    val state: String,
+)
+
+data class TokenResponse(
+    @SerializedName("grant_type") val grantType: String,
+    val accessToken: String,
+    val refreshToken: String,
+    val accessTokenExpiresIn: String,
+    val refreshTokenExpiresIn: String
+)
+
+data class ExtraDataBody(
+    val school: String,
+    val grade: String,
+    val spot: String
+)
