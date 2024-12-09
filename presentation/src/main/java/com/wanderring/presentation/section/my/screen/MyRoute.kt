26698cde02046b690df257bc7eda728419ecf63d
youloveli <mymhj122@gmail.com>
2024-12-09 23:14:56 +0900

    navihost에서 제공하는 함수들 사용해서 바텀시트 띄우기 구현

--- presentation/src/main/java/com/wanderring/presentation/section/my/screen/MyRoute.kt
+++ presentation/src/main/java/com/wanderring/presentation/section/my/screen/MyRoute.kt
@@ -1,5 +1,6 @@
 package com.wanderring.presentation.section.my.screen
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,50 +8,68 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ModalBottomSheetLayout
-import androidx.compose.material.ModalBottomSheetValue
-import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.material.ModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.repeatOnLifecycle
 import com.wanderring.domain.model.enumType.Gender
 import com.wanderring.domain.model.enumType.Grade
 import com.wanderring.domain.model.enumType.Tag
 import com.wanderring.presentation.section.home.component.DoWalkListItem
 import com.wanderring.presentation.section.home.component.DoWalkListItemState
-import com.wanderring.presentation.section.my.component.AlamBottomSheetContent
 import com.wanderring.presentation.section.my.viewModel.MyIntent
 import com.wanderring.presentation.section.my.viewModel.MySideEffect
 import com.wanderring.presentation.section.my.viewModel.MyState
 import com.wanderring.presentation.section.my.viewModel.MyViewModel
 import com.wanderring.presentation.section.onboarding.component.ProfileCard
+import com.wanderring.presentation.utill.BottomSheetType
 import com.wanderring.presentation.utill.DoPreview
 import kotlinx.collections.immutable.immutableListOf
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun MyRoute(
     modifier: Modifier = Modifier,
     myViewModel: MyViewModel = hiltViewModel(),
+    bottomSheetState: ModalBottomSheetState,
     navigateProfile: () -> Unit,
+    updateBottomSheetType: (BottomSheetType) -> Unit,
 ) {
     val myState by myViewModel.state.collectAsStateWithLifecycle()
+    val coroutineScope = rememberCoroutineScope()
 
-    LaunchedEffect(Unit) {
-        myViewModel.sideEffect.collect {
-            when (it) {
-                MySideEffect.NavigateProfile -> navigateProfile()
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+    LaunchedEffect(lifecycle) {
+        lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            myViewModel.sideEffect.collect { effect ->
+                when (effect) {
+                    is MySideEffect.NavigateProfile -> navigateProfile()
+                    is MySideEffect.HideBottomSheet -> {
+                         }
+                    is MySideEffect.ShowBottomSheet -> {
+                        Log.d("sideEffect", "collat $effect")
+                        updateBottomSheetType(BottomSheetType.MyPageOption)
+                        coroutineScope.launch {
+                            Log.d("coroutineScope sideEffect", "collat $effect")
+                            bottomSheetState.show()
+                        }
+                    }
+                }
             }
         }
     }
+
 
     MyScreen(
         modifier = modifier,
