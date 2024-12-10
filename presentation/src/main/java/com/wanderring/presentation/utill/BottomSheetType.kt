85f5b60210217e58913897ed2e29f730536a850c
youloveli <mymhj122@gmail.com>
2024-12-10 13:57:07 +0900

    BottomSheetType 에서 바텀시트에서 실행시킬 함수를 받도록 변경

--- presentation/src/main/java/com/wanderring/presentation/utill/BottomSheetType.kt
+++ presentation/src/main/java/com/wanderring/presentation/utill/BottomSheetType.kt
@@ -1,15 +1,57 @@
 package com.wanderring.presentation.utill
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.wanderring.presentation.section.my.component.AlamBottomSheetContent
 
-enum class BottomSheetType(val bottomSheet: @Composable () -> Unit) {
-    None(bottomSheet = {}),
-    HomeTagFilter(bottomSheet = {}),
-    HomeLocationFilter(bottomSheet = {}),
-    WriteWatchReport(bottomSheet = {}),
-    MyWriteWatch(bottomSheet = {}),
-    MyPageOption(bottomSheet = {
-        AlamBottomSheetContent()
-    }),
+sealed class BottomSheetType {
+    @Composable
+    abstract fun content()
+
+    data object None : BottomSheetType() {
+        @Composable
+        override fun content() {
+        }
+    }
+
+    data class HomeTagFilter(val modifier: Modifier = Modifier) : BottomSheetType() {
+        @Composable
+        override fun content() {
+        }
+    }
+
+    data class HomeLocationFilter(val modifier: Modifier = Modifier) : BottomSheetType() {
+        @Composable
+        override fun content() {
+        }
+    }
+
+    data class WriteWatchReport(val modifier: Modifier = Modifier) : BottomSheetType() {
+        @Composable
+        override fun content() {
+        }
+    }
+
+    data class MyWriteWatch(val modifier: Modifier = Modifier) : BottomSheetType() {
+        @Composable
+        override fun content() {
+        }
+    }
+
+    data class MyPageOption(
+        val modifier: Modifier = Modifier,
+        val profileChangeOnClick: () -> Unit,
+        val logoutOnClick: () -> Unit,
+        val cancelOnClick: () -> Unit,
+    ) : BottomSheetType() {
+        @Composable
+        override fun content() {
+            AlamBottomSheetContent(
+                modifier = modifier,
+                profileChangeOnClick = profileChangeOnClick,
+                logoutOnClick = logoutOnClick,
+                cancelOnClick = cancelOnClick,
+            )
+        }
+    }
 }
