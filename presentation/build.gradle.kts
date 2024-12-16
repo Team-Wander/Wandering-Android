00b208ccbb873feb03b4dbcc94a5c0ac3316cdfa
youloveli <mymhj122@gmail.com>
2024-12-16 11:08:30 +0900

    네이버 로그로그인 라이브러리 추가

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -11,7 +11,6 @@ android {
 
     defaultConfig {
         minSdk = ProjectProperties.Versions.MIN_SDK
-
         testInstrumentationRunner = ProjectProperties.Test.TEST_RUNNER
         consumerProguardFiles(ProjectProperties.Files.CONSUMER_PROGUARD_FILES)
     }
@@ -25,16 +24,20 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = ProjectProperties.Versions.JAVA_VERSION
         targetCompatibility = ProjectProperties.Versions.JAVA_VERSION
     }
+
     kotlinOptions {
         jvmTarget = ProjectProperties.Versions.JVM_TARGET
     }
+
     composeOptions {
         kotlinCompilerExtensionVersion = Versions.COMPOSE
     }
+
     buildFeatures {
         compose = true
         buildConfig = true
@@ -48,10 +51,8 @@ dependencies {
     implementation(Dependency.AndroidX.LIFECYCLE)
     implementation(Dependency.AndroidX.SPLASH)
 
-
     implementation(Dependency.Compose.ACTIVITY_COMPOSE)
     implementation(platform(Dependency.Compose.COMPOSE_BOM))
-    implementation(Dependency.AndroidX.COMPOSE_LIFECYCLE)
     implementation(Dependency.Compose.COMPOSE)
     implementation(Dependency.Compose.COMPOSE_GRAPHICS)
     implementation(Dependency.Compose.COMPOSE_PREVIEW)
@@ -67,12 +68,18 @@ dependencies {
     implementation(Dependency.Hilt.HILT_NAVIGATION_FRAGMENT)
     implementation(Dependency.Hilt.HILT_NAVIGATION_COMPOSE)
 
+    implementation(Dependency.Retrofit.RETROFIT)
+    implementation(Dependency.Retrofit.RETROFIT_CONVERTER_GSON)
+    implementation(Dependency.OkHttp.OKHTTP_LOGGING_INTERCEPTOR)
+    implementation(Dependency.Coil.COIL)
+    implementation(files("libs/oauth-5.10.0.aar"))
+
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
+    implementation("com.airbnb.android:lottie:3.1.0")
+
     testImplementation(Dependency.Test.JUNIT)
     androidTestImplementation(Dependency.Test.ANDROID_JUNIT)
     androidTestImplementation(Dependency.Test.ESPRESSO)
-    androidTestImplementation(platform(Dependency.Compose.COMPOSE_BOM))
     androidTestImplementation(Dependency.Test.COMPOSE_JUNIT)
     debugImplementation(Dependency.Test.COMPOSE_MANIFEST)
-
-    implementation(Dependency.Coil.COIL)
 }
