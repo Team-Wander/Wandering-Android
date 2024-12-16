dcfce58df18e25ac2e2a088f649e2d3cc33b0752
youloveli <mymhj122@gmail.com>
2024-12-16 12:30:16 +0900

    오류 수정

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -76,6 +76,8 @@ dependencies {
 
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
     implementation("com.airbnb.android:lottie:3.1.0")
+    implementation(Dependency.AndroidX.COMPOSE_LIFECYCLE)
+    androidTestImplementation(platform(Dependency.Compose.COMPOSE_BOM))
 
     testImplementation(Dependency.Test.JUNIT)
     androidTestImplementation(Dependency.Test.ANDROID_JUNIT)
